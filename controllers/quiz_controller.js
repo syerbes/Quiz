ee52938f430eca40755b650324f2836ba46e0bc8
Sergio <syerbes@outlook.com>
2016-05-04 02:54:20 +0200

    Base de Datos Sequelize

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,36 @@
+var models = require('../models');
+
 // GET /quizes/question
  exports.question = function(req, res, next) {
-
- 	var answer = req.query.answer || '';
-    res.render('quizes/question', {question: 'Capital de Italia', answer: answer});
+ 	models.Quiz.findOne()
+ 		.then(function(quiz){
+ 			if (quiz) {
+ 				var answer = req.query.answer || '';
+ 				res.render('quizes/question', {question: quiz.question, answer: answer});
+ 			}
+ 			else {
+ 				throw new Error('No hay preguntas en la BBDD.');
+ 			}
+ 		}).catch(function(error) {
+ 			next(error);
+ 		});
  };
  
  // GET /quizes/answer
  exports.check = function(req, res, next) {
-
- 	var answer = req.query.answer || '';
- 	var result = ((answer === 'Roma') ? 'Correcta' : 'Incorrecta');
- 	res.render('quizes/result', {result: result, answer: answer});
+ 	models.Quiz.findOne()
+ 		.then(function(quiz){
+ 			if (quiz) {
+ 				var answer = req.query.answer || '';
+ 				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+ 				res.render('quizes/result', { result: result, answer: answer});
+ 			}
+ 			else {
+ 				throw new Error('No hay preguntas en la BBDD.');
+ 			}
+ 		}).catch(function(error) {
+ 			next(error);
+ 		});
  };
 
  exports.author = function (req, res) {
